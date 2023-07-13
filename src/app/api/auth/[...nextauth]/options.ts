ffd3f7defaa192a93c1c816c81663354ab0ded42
Neginuko <hirahaya0117@gmail.com>
2023-07-13 09:28:49 +0900

    move and fix ENV names

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,14 +5,15 @@ import GoogleProvider from "next-auth/providers/google";
 export const options: NextAuthOptions = {
   debug: true,
   session: {strategy: "jwt"},
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     // GitHubProvider({
     //     clientId: process.env.GITHUB_ID!,
     //     clientSecret: process.env.GITHUB_SECRET!,
     // }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_SECRET_ID!,
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_SECRET_ID ?? '',
     }),
   ],
   callbacks: {
