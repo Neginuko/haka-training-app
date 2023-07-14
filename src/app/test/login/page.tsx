d7dbbfc1d0b3d21e63098e408b7897baee2b0301
Neginuko <hirahaya0117@gmail.com>
2023-07-14 09:13:05 +0900

    for vercel test page

---
+++ src/app/test/login/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import { LoginButton, LogoutButton } from '../components/auth_buttons';
+
+export default function Home() {
+    const  { data: session } = useSession();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Hello&nbsp;
+          <code className="font-mono font-bold">{session?.user?.name ?? 'guest'}</code>
+        </p>
+        {!session && (
+            <LoginButton></LoginButton>
+        )}
+        {session && (
+            <LogoutButton></LogoutButton>
+        )}
+      </div>
+    </main>
+  )
+}
