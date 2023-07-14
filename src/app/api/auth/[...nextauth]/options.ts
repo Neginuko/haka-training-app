7a5072ebdc43247d70292f8e1fff17d2325ec331
Neginuko <hirahaya0117@gmail.com>
2023-07-14 09:11:14 +0900

    add prisma adapter to auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,16 @@
 import type { NextAuthOptions } from "next-auth";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
 
 export const options: NextAuthOptions = {
   debug: true,
   session: {strategy: "jwt"},
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
   providers: [
     // GitHubProvider({
     //     clientId: process.env.GITHUB_ID!,
