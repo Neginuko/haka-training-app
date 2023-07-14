41fcab28750e54c63ff26cba0fa39afb82a229e9
bearone236 <kuma_2306@icloud.com>
2023-07-14 21:35:29 +0900

    Login Design Complete

--- src/app/test/components/auth_buttons.tsx
+++ src/app/test/components/auth_buttons.tsx
@@ -1,15 +1,29 @@
-"use client";
-
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut } from 'next-auth/react';
 
 export const LoginButton = () => {
-    return (
-        <button className='flex w-full justify-center border-2 border-b border-red-300 hover:border-red-400 bg-red-200 hover:bg-red-300 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:hover:border-red-900 dark:bg-red-800/50 dark:hover:bg-red-900/30 lg:static lg:w-auto lg:rounded-xl lg:p-4' onClick={() => signIn()}>サインイン</button>
-    );
+  const handleSignIn = () => {
+    signIn();
+  };
+
+  return (
+    <div className="login">
+      <button className="login-button" onClick={handleSignIn}>
+        サインイン
+      </button>
+    </div>
+  );
 };
 
 export const LogoutButton = () => {
-    return (
-        <button className='flex w-full justify-center border-2 border-b border-green-300 bg-green-200 pb-6 pt-8 backdrop-blur-2xl dark:border-green-800 dark:bg-green-800/30 lg:static lg:w-auto lg:rounded-xl lg:p-4 hover:border-green-400 hover:bg-green-300 dark:hover:border-green-900 dark:hover:bg-green-900/30' onClick={() => signOut()}>サインアウト</button>
-    );
+  const handleSignOut = () => {
+    signOut();
+  };
+
+  return (
+    <div className="login">
+      <button className="login-button" onClick={handleSignOut}>
+        サインアウト
+      </button>
+    </div>
+  );
 };
