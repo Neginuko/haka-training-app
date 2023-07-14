85908c41134990ce9041123f54db789b33a5aaf0
Neginuko <hirahaya0117@gmail.com>
2023-07-14 17:14:40 +0900

    レコードに登録するAPIの追加

---
+++ src/app/api/userdata/commit.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma";
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+    const req = JSON.parse(request.body);
+
+    try {
+        const data = await prisma.record.create({
+            data: {
+                userId: req.userId,
+                trainingId: req.trainingId,
+                doneTimes: req.times,
+            }
+        });
+
+        return response.status(200).json({ data });
+    } catch(error) {
+        return response.status(500).json({ error });
+    }
+}
