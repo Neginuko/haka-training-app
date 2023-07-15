2d8836eef4064c0973ea50457c651fdf70b2c68c
Neginuko <hirahaya0117@gmail.com>
2023-07-15 22:53:02 +0900

    adds some utils

--- src/util/api.ts
+++ src/util/api.ts
@@ -13,7 +13,6 @@ export type Commit = {
 
 export async function getCommitsFromUserId(id: string) {
     try {
-
         const rdata = await prisma.record.findMany({
             where: {
                 userId: id,
@@ -43,6 +42,29 @@ export async function getCommitsFromUserId(id: string) {
     }
 }
 
+export async function getCommitsFromUserIdOnDate(userId: string, date: Date) {
+    try {
+        const data = await prisma.record.findMany({
+            where: {
+                userId: userId,
+                date: {
+                    gte: date,
+                    lt: new Date(date.setDate(date.getDate() + 1)),
+                },
+            },
+            select: {
+                id: true,
+                date: true,
+                trainingId: true,
+                doneTimes: true,
+            },
+        });
+        return data;
+    } catch (error) {
+
+    }
+}
+
 export async function getTrainingCommitCount(userId: string) {
     try {
         const rdata = await getCommitsFromUserId(userId) ?? [];
@@ -53,9 +75,9 @@ export async function getTrainingCommitCount(userId: string) {
         for (let item of rdata) {
             const trainingName = await getNameFromTrainingId(item.trainingId!);
             if (!Object.keys(data).some((v) => v === trainingName)) {
-                data[trainingName] = item.doneTimes ?? 0;
+                data[trainingName ?? "__"] = item.doneTimes ?? 0;
             } else {
-                data[trainingName] += item.doneTimes ?? 0;
+                data[trainingName ?? "__"] += item.doneTimes ?? 0;
             }
         }
         return data;
@@ -99,30 +121,33 @@ export async function getNameFromTrainingId(trainingId: string) {
 }
 
 export async function getAverageOnDate(date: Date) {
-    const fdate = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
-
+    const nextdate = new Date(date);
+    nextdate.setDate(date.getDate() + 1);
     try {
         const commits = await prisma.record.findMany({
             select: {
                 id: true,
                 trainingId: true,
                 doneTimes: true,
+            },
+            where: {
+                date: {
+                    gte: date,
+                    lt: nextdate,
+                },
             }
         });
         interface fmt {
-            id: string,
-            trainingId: string,
-            trainingName: string,
-            doneTimes: number,
+            [propName: string]: number,
         };
-        const data: fmt[] = [];
+        let data: fmt = {};
         for (let item of commits) {
-            data.push({
-                id: item.id,
-                trainingId: item.trainingId,
-                trainingName: await getNameFromTrainingId(item.trainingId) ?? "",
-                doneTimes: item.doneTimes,
-            });
+            const trainName = await getNameFromTrainingId(item.trainingId);
+            if (!Object.keys(data).some( v => v === trainName)) {
+                data[trainName ?? "__"] = item.doneTimes;
+            } else {
+                data[trainName ?? "__"] += item.doneTimes;
+            }
         }
         return data;
     } catch (error) {
@@ -130,6 +155,74 @@ export async function getAverageOnDate(date: Date) {
     }
 }
 
-export async function calcScores(userId: string) {
-    
+export async function getRatingFromUserId(userId: string) {
+    try {
+        const data = await prisma.rating.findMany({
+            select: {
+                date: true,
+                rate: true,
+            },
+            where: {
+                userId: userId
+            }
+        });
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function setRatingToUserIdOnDate(userId: string, date: Date) {
+    const nextdate = new Date(date);
+    nextdate.setDate(date.getDate() + 1);
+    const prevdate = new Date(date);
+    prevdate.setDate(date.getDate() - 1);
+    try {
+        const currentRate = await prisma.rating.findFirst({
+            select: {
+                rate: true
+            },
+            where: {
+                date: {
+                    gte: prevdate,
+                    lt: date,
+                }
+            },
+        });
+        const users = await prisma.user.count();
+        const commitsAvg = await getAverageOnDate(prevdate);
+        let allAvg = 0;
+        for (let item in commitsAvg) {
+            allAvg += commitsAvg.item;
+        }
+        allAvg /= users;
+        const value = 0;
+        const userData = await getCommitsFromUserIdOnDate(userId, prevdate);
+        for (let item of userData!) {
+            item.doneTimes += value;
+        }
+        const rate = rating(allAvg, value, currentRate!.rate);
+        prisma.rating.create({
+            data: {
+                userId: userId,
+                rate: rate,
+            }
+        });
+    } catch (error) {}
+}
+
+function rating(allAvg: number, value: number, currentRate: number) {
+    const K = 16;
+    const rrate = K / (10 ** ((Math.max(allAvg, value) - Math.min(allAvg, value)) / 100) + 1);
+    const rate = clamp(rrate, 0.2, 64);
+    return Math.ceil(currentRate + rate + 0.5);
+}
+
+function clamp(rrate: number, min: number, max: number) {
+    if (rrate < min) {
+        return min;
+    } else if (rrate > max) {
+        return max;
+    }
+    return rrate;
 }
