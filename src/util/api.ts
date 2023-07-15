1be46d4435b27196c8ef5ceee85d5343ff9fc246
Neginuko <hirahaya0117@gmail.com>
2023-07-15 18:48:28 +0900

    try catch wrapping

--- src/util/api.ts
+++ src/util/api.ts
@@ -12,78 +12,90 @@ export type Commit = {
 };
 
 export async function getCommitsFromUserId(id: string) {
-    const rdata = await prisma.record.findMany({
-        where: {
-            userId: id,
-        },
-        select: {
-            id: true,
-            date: true,
-            trainingId: true,
-            doneTimes: true,
-        },
-    });
-    let data = []
-    for (let i=0; i<rdata.length; i++) {
-        let r = rdata[i];
-        let t = rdata[i].date;
-        let rr: Commit = {
-            id: r.id,
-            date: t.getFullYear() + "/" + t.getMonth() + "/" + t.getDate(),
-            trainingId: r.trainingId,
-            doneTimes: r.doneTimes,
-        };
-        data.push(rr);
-    }
+    try {
 
-    return data;
+        const rdata = await prisma.record.findMany({
+            where: {
+                userId: id,
+            },
+            select: {
+                id: true,
+                date: true,
+                trainingId: true,
+                doneTimes: true,
+            },
+        });
+        let data = []
+        for (let i=0; i<rdata.length; i++) {
+            let r = rdata[i];
+            let t = rdata[i].date;
+            let rr: Commit = {
+                id: r.id,
+                date: t.getFullYear() + "/" + t.getMonth() + "/" + t.getDate(),
+                trainingId: r.trainingId,
+                doneTimes: r.doneTimes,
+            };
+            data.push(rr);
+        }
+        return data;
+    } catch (error) {
+        return null;
+    }
 }
 
 export async function getTrainingCommitCount(userId: string) {
-    const rdata = await getCommitsFromUserId(userId);
-
-    interface fmt {
-        [propName: string]: number,
-    };
-    const data: fmt = {};
-    for (let item of rdata) {
-        const trainingName = await getNameFromTrainingId(item.trainingId!);
-        if (!Object.keys(data).some((v) => v === trainingName)) {
-            data[trainingName] = item.doneTimes ?? 0;
-        } else {
-            data[trainingName] += item.doneTimes ?? 0;
+    try {
+        const rdata = await getCommitsFromUserId(userId) ?? [];
+        interface fmt {
+            [propName: string]: number,
+        };
+        const data: fmt = {};
+        for (let item of rdata) {
+            const trainingName = await getNameFromTrainingId(item.trainingId!);
+            if (!Object.keys(data).some((v) => v === trainingName)) {
+                data[trainingName] = item.doneTimes ?? 0;
+            } else {
+                data[trainingName] += item.doneTimes ?? 0;
+            }
         }
+        return data;
+    } catch (error) {
+        return null;
     }
-
-    return data;
 }
 
 export async function getDailyCommitCount(userId: string) {
-    const rdata = await getCommitsFromUserId(userId);
+    try {
 
-    interface fmt {
-        [propName: string]: number,
-    };
-    const data: fmt = {};
-    for(let item of rdata) {
-        if (!Object.keys(data).some((v) => v === item.date)) {
-            data[item.date ?? "__"] = 1;
-        } else {
-            data[item.date ?? "__"] += 1;
+        const rdata = await getCommitsFromUserId(userId) ?? [];
+        interface fmt {
+            [propName: string]: number,
+        };
+        const data: fmt = {};
+        for(let item of rdata) {
+            if (!Object.keys(data).some((v) => v === item.date)) {
+                data[item.date ?? "__"] = 1;
+            } else {
+                data[item.date ?? "__"] += 1;
+            }
         }
+        return data;
+    } catch (error) {
+        return null;
     }
-
-    return data;
 }
 
 export async function getNameFromTrainingId(trainingId: string) {
-    const result = await prisma.training.findUnique({
-        where: {
-            id: trainingId,
-        }
-    });
-
-    return result?.name ?? "";
+    try {
+        const result = await prisma.training.findUnique({
+            where: {
+                id: trainingId,
+            }
+        });
+        return result?.name ?? "";
+    } catch (error) {
+        return null;
+    }
 }
 
 export async function getAverageOnDate(date: Date) {
@@ -108,7 +120,7 @@ export async function getAverageOnDate(date: Date) {
             data.push({
                 id: item.id,
                 trainingId: item.trainingId,
-                trainingName: await getNameFromTrainingId(item.trainingId),
+                trainingName: await getNameFromTrainingId(item.trainingId) ?? "",
                 doneTimes: item.doneTimes,
             });
         }
