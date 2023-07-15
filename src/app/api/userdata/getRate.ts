b4857d78229ed36a5192b45fcdb0684d4b350b7b
Neginuko <hirahaya0117@gmail.com>
2023-07-15 22:58:39 +0900

    API

--- src/app/api/userdata/getRate.ts
+++ src/app/api/userdata/getRate.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getCommitsFromUserId } from "../../../util/api";
+import { getRatingFromUserId } from "../../../util/api";
 
 export async function handler(request: NextApiRequest, response: NextApiResponse) {
     const req = JSON.parse(request.body);
 
     try {
-        const data = await getCommitsFromUserId(req.userId);
+        const data = await getRatingFromUserId(req.userId);
         response.status(200).json({ data });
     } catch(error) {
         response.status(500).json({ error });
--- src/app/api/userdata/rating.ts
+++ src/app/api/userdata/rating.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getCommitsFromUserId } from "../../../util/api";
+import { setRatingToUserIdOnDate } from "../../../util/api";
 
 export async function handler(request: NextApiRequest, response: NextApiResponse) {
     const req = JSON.parse(request.body);
 
     try {
-        const data = await getCommitsFromUserId(req.userId);
+        const data = await setRatingToUserIdOnDate(req.userId, new Date());
         response.status(200).json({ data });
     } catch(error) {
         response.status(500).json({ error });
